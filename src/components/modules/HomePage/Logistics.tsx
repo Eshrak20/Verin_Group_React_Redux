81dcb899f46706f75ca306ea4b61abdda14ed63f
Md Nazmul Islam <nazmulislam5691@gmail.com>
2026-07-24 22:28:37 +0600

    update static add to card and wishlist

--- src/components/modules/HomePage/Logistics.tsx
+++ src/components/modules/HomePage/Logistics.tsx
@@ -17,26 +17,3 @@ export default function Logistics() {
 }
 
 
-
-
-
-
-
-
-
-// import ClientTestimonials from "./ClientTestimonials";
-// import OurClients from "./OurClients";
-
-
-// export default function Logistics() {
-//   return (
-//     <div className="max-w-6xl mx-auto w-full">
-//         <div className="flex justify-center">
-//             <h1 className="text-4xl home-black-text pt-5 font-bold">Logistics zone</h1>
-//         </div>
-//       <OurClients />
-//       <div className="border-t border-gray-200 dark:border-gray-700 mx-6" />
-//       <ClientTestimonials />
-//     </div>
-//   );
-// }
--- src/types/product.type.ts
+++ src/types/product.type.ts
@@ -87,30 +87,3 @@ export interface ProductsResponse {
 
 
 
-
-
-// export interface Product {
-//   id: number;
-//   slug: string;
-//   name: string;
-//   shortDescription: string;
-//   description: string;
-//   image: string;
-//   images: string[];
-//   price: number;
-//   oldPrice?: number;
-//   discount?: number;
-//   rating: number;
-//   reviews: number;
-//   stock: number;
-//   sku: string;
-//   brand: string;
-//   category: string;
-//   subCategory: string;
-//   tags: string[];
-//   isFeatured: boolean;
-//   isNew: boolean;
-//   isHotSell: boolean;
-//   sizes: string[];
-//   colors: { name: string; code: string }[];
-// }
