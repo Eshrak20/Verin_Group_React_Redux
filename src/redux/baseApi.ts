3a07d4e12d8e931c04bf35950220435bf505f88d
eshrak20 <uemran2018@gmail.com>
2026-09-18 17:11:17 +0600

    working on baseApi

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://v.veringroup.com/api/v1",
+    baseUrl: "https://test.veringroup.com/api/v1",
     prepareHeaders: (headers) => {
       headers.set("Accept", "application/json");
       return headers;
